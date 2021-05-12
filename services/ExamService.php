35ea5467c3f29e97d8652e33bb0ecd8403c7b5c4
lndex0 <dolinay10@gmail.com>
2021-05-12 12:50:56 +0200

    create_exam test

--- services/ExamService.php
+++ services/ExamService.php
@@ -15,8 +15,219 @@ class ExamService
     //                                   Teacher part
     // -----------------------------------------------------------------------------------------------
 
-    public function create_exam($resp)
+    public function create_exam($data)
     {
+
+        if(isset($data['code'])){
+            if(!empty($data['code'])){
+                $code= $data['code'];
+            }
+        }
+        if(isset($data['creator'])){
+            if(!empty($data['creator'])) {
+                $id_creator = $data['creator'];
+            }
+        }
+        if(isset($data['$start'])){
+            if(!empty($data['start'])) {
+                $start = $data['$start'];
+            }
+        }
+
+        if(isset($data['end'])){
+            if(!empty($data['end'])) {
+                $end = $data['end'];
+            }
+        }
+        if(isset($data['description'])) {
+            if (!empty($data['name'])) {
+                $name = $data['description'];
+            }
+        }
+
+
+        $stmt = $this->conn->prepare('insert into exams (id_creator, code, name, start, end) values (:id_creator, :code, :name, :start, :end)');
+        $stmt->bindParam('id_creator', $id_creator);
+        $stmt->bindParam('code', $code);
+        $stmt->bindParam('name', $name);
+        $stmt->bindParam('start', $start);
+        $stmt->bindParam('end', $end);
+
+        if(!$stmt->execute()){
+            $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+        }
+        else{
+            $id_exam = $this->conn->lastInsertId();
+        }
+
+        if(isset($data['qShort'])){
+        if (!empty($data['qShort'])) {
+            foreach ($data['qShort'] as $item) {
+                if(isset($item['description']) && isset($item['score']) && isset($item['answer'])) {
+                    if(!empty($item['description']) && !empty($item['score']) && !empty($item['answer'])) {
+                        $name = $item['description'];
+                        $score = $item['score'];
+                        $answer = $item['answer'];
+                        $id_type = 2;
+                        $stmt_qShort = $this->conn->prepare('insert into questions (id_exam, id_type, name, score) values (:id_exam, :id_type, :name, :score)');
+                        $stmt_qShort->bindParam('id_exam', $id_exam);
+                        $stmt_qShort->bindParam('id_type', $id_type);
+                        $stmt_qShort->bindParam('name', $name);
+                        $stmt_qShort->bindParam('score', $score);
+                        if (!$stmt_qShort->execute()) {
+                            $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                            break;
+                        } else {
+                            $id_question = $this->conn->lastInsertId();
+                        }
+                        $stmt_qShort_answer = $this->conn->prepare('insert into questions_short(id_question, answer) values (:id_question, :answer)');
+                        $stmt_qShort_answer->bindParam('id_question', $id_question);
+                        $stmt_qShort_answer->bindParam('answer', $answer);
+                        if (!$stmt_qShort_answer->execute()) {
+                            $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+        if(isset($data['qSelect'])){
+            if(!empty($data['qSelect'])){
+                foreach ($data['qSelect'] as $item) {
+                    if(isset($item['description']) && isset($item['score']) && isset($item['correctAnswer']) && isset($item['possibilities'])) {
+                        if(!empty($item['description']) && !empty($item['score']) && !empty($item['correctAnswer']) && !empty($item['possibilities'])) {
+                            $name = $item['description'];
+                            $score = $item['score'];
+                            $correctAnswer = $item['correctAnswer'];
+                            $id_type = 1;
+                            $stmt_qSelect = $this->conn->prepare('insert into questions (id_exam, id_type, name, score) values (:id_exam, :id_type, :name, :score)');
+                            $stmt_qSelect->bindParam('id_exam', $id_exam);
+                            $stmt_qSelect->bindParam('id_type', $id_type);
+                            $stmt_qSelect->bindParam('name', $name);
+                            $stmt_qSelect->bindParam('score', $score);
+                            if (!$stmt_qSelect->execute()) {
+                                $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                                break;
+                            } else {
+                                $id_question = $this->conn->lastInsertId();
+                            }
+                            foreach ($item['possibilities'] as $value) {
+                                if(isset($value['answer'])){
+                                    if(!empty($value['answer'])){
+                                        $answer = $value['answer'];
+                                        $stmt_qSelect_answer = $this->conn->prepare('insert into questions_short(id_question, answer, correct) values (:id_question, :answer, :correct)');
+                                        $stmt_qSelect_answer->bindParam('id_question', $id_question);
+                                        $stmt_qSelect_answer->bindParam('answer', $answer);
+                                        $stmt_qSelect_answer->bindParam('correct', $correctAnswer);
+                                        if (!$stmt_qSelect_answer->execute()) {
+                                            $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isset($data['qImage'])){
+            if(!empty($data['qImage'])){
+                foreach ($data['qImage'] as $item){
+                    if(isset($data['description']) && isset($data['score'])){
+                        if(!empty($data['description']) && !empty($data['score'])){
+                            $name = $item['description'];
+                            $score = $item['score'];
+                            $id_type = 3;
+                            $stmt_qImage = $this->conn->prepare('insert into questions  (id_exam, id_type, name, score) values (:id_exam, :id_type, :name, :score)');
+                            $stmt_qImage->bindParam('id_exam', $id_exam);
+                            $stmt_qImage->bindParam('id_type', $id_type);
+                            $stmt_qImage->bindParam('name', $name);
+                            $stmt_qImage->bindParam('score', $score);
+                            if (!$stmt_qImage->execute()) {
+                                $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isset($data['qEquation'])){
+            if(!empty($data['qEquation'])){
+                foreach ($data['qEquation'] as $item){
+                    if(isset($item['description']) && isset($item['score'])){
+                        if(!empty($item['description']) && !empty($item['score'])){
+                            $name = $item['description'];
+                            $score = $item['score'];
+                            $id_type = 4;
+                            $stmt_qEquation = $this->conn->prepare('insert into questions  (id_exam, id_type, name, score) values (:id_exam, :id_type, :name, :score)');
+                            $stmt_qEquation->bindParam('id_exam', $id_exam);
+                            $stmt_qEquation->bindParam('id_type', $id_type);
+                            $stmt_qEquation->bindParam('name', $name);
+                            $stmt_qEquation->bindParam('score', $score);
+                            if (!$stmt_qEquation->execute()) {
+                                $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isset($data['qPairs'])){
+            if(!empty($data['qPairs'])){
+                foreach ($data['qPairs'] as $item){
+                    if(isset($item['description']) && isset($item['score'])){
+                        if(!empty($item['description']) && !empty($item['score'])){
+                            $name = $item['description'];
+                            $score = $item['score'];
+                            $id_type = 5;
+                            $stmt_qPairs = $this->conn->prepare('insert into questions (id_exam, id_type, name, score) values (:id_exam, :id_type, :name, :score)');
+                            $stmt_qPairs->bindParam('id_exam', $id_exam);
+                            $stmt_qPairs->bindParam('id_type', $id_type);
+                            $stmt_qPairs->bindParam('name', $name);
+                            $stmt_qPairs->bindParam('score', $score);
+                            if (!$stmt_qPairs->execute()) {
+                                $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                                break;
+                            } else {
+                                $id_question = $this->conn->lastInsertId();
+                            }
+                            if(isset($item['answers'])){
+                                if(!empty($item['answers'])){
+                                    foreach ($item['answers'] as $value){
+                                        if(isset($value['left']) && isset($value['right'])) {
+                                            if(!empty($value['left']) && !empty($value['right'])) {
+                                                $answer_left = $value['left'];
+                                                $answer_right = $value['right'];
+                                                $stmt = $this->conn->prepare('insert into questions_pairing(id_question, answer_left, answer_right) values (:id_question, :answer_left, :answer_right)');
+                                                $stmt->bindParam('"id_question', $id_question);
+                                                $stmt->bindParam("answer_left", $answer_left);
+                                                $stmt->bindParam('answer_right', $answer_right);
+                                                if(!$stmt->execute()){
+                                                    $resp = ['status' => 'FAIL', 'message' => 'create_exam_exams_insert'];
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
         $resp = ['status' => 'FAIL', 'message' => 'create_exam'];
         return json_encode($resp);
     }
