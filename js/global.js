cad3125bb3b463588713abc9f1676b5448adebf5
Adam Klimko <adam.klimko@gmail.com>
2021-05-14 19:52:15 +0200

    commit

--- js/global.js
+++ js/global.js
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-//  var server = 'http://147.175.98.107/zaver/';
-var server = 'https://wt76.fei.stuba.sk/zaverecne/';
-=======
- //var server = 'http://147.175.98.107/zaver/';
-//var server = 'https://wt76.fei.stuba.sk/zaverecne/';
-var server = 'https://147.175.98.55/WebteProjekt/';
->>>>>>> dca95551da21c7c3ac3fa94c959186740cb47c78
+ var server = 'http://147.175.98.107/zaver/';
+// var server = 'https://wt76.fei.stuba.sk/zaverecne/';
