dca95551da21c7c3ac3fa94c959186740cb47c78
Marek <grezdom@gmail.com>
2021-05-14 19:45:44 +0200

    Delete changes

--- js/global.js
+++ js/global.js
@@ -1,2 +1,3 @@
- var server = 'http://147.175.98.107/zaver/';
+ //var server = 'http://147.175.98.107/zaver/';
 //var server = 'https://wt76.fei.stuba.sk/zaverecne/';
+var server = 'https://147.175.98.55/WebteProjekt/';
